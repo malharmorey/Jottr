d6a521dd34a6e3bee649da948b3202ecbd5e176a
Malhar <malharmorey@gmail.com>
2026-05-26 20:29:46 +0530

    security: cap json body at 18kb

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 8080;
 
 //Middleware function
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: '18kb' }));
 app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
 
 // Available Routes
@@ -27,6 +27,13 @@ app.use('/api/notes', notesRouter);
 
 app.get('/', (req, res) => {
 	res.status(200).send('Welcome to server!');
+});
+
+app.use((err, req, res, next) => {
+	if (err.type === 'entity.too.large') {
+		return res.status(413).json({ success: false, message: 'Request too large' });
+	}
+	next(err);
 });
 
 app.listen(port, () => {
