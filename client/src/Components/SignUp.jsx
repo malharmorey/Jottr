d6d31a0c683978f2d7f9573483c1543436fc6510
Malhar <malharmorey@gmail.com>
2026-06-19 22:43:36 +0530

    fix: even out signup field spacing on mobile

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -57,15 +57,11 @@ const SignUp = ({ title }) => {
 									name='email'
 									className='form-control inputField'
 									id='email'
-									aria-describedby='emailHelp'
 									placeholder='john@example.com'
 									value={credentials.email}
 									onChange={onChange}
 									required
 								/>
-								<div id='emailHelp' className='form-text'>
-									We'll never share your email with anyone else.
-								</div>
 							</div>
 							<div className='mb-3'>
 								<label htmlFor='password' className='form-label'>
